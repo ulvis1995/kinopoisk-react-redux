7e85d64245dfac03a01e424587d9f33524641ae9
ViolettaParamonova <violettapar1995@gmail.com>
2022-07-15 11:44:03 +0300

    connetc redux-persist to save application state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,25 @@
 import { configureStore, compose, applyMiddleware } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
-import thunk from "redux-thunk";
+import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
 
-const store = configureStore(
-  {reducer: rootReducer},
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig,  rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({serializableCheck: false})
+  },
   composeEnhancers(applyMiddleware(thunk))
 );
-store.subscribe(() => console.log(store.getState()))
+
+export const persistor = persistStore(store);
 export default store;
